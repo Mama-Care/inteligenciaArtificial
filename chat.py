7cd0951be923c70a8512a1f8792d3b43dfd4a457
Paulo Meneghini <paulo.meneghini@outlook.com>
2024-11-10 21:39:38 -0300

    implementing the rules for questions

--- chat.py
+++ chat.py
@@ -8,6 +8,7 @@ from langchain_community.llms import Ollama
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import streamlit as st
 
 #pip install langchain-huggingface
@@ -19,43 +20,74 @@ from langchain_community.document_loaders import PyPDFLoader
 #%pip install -qU  langchain_milvus
 from langchain_milvus import Milvus
 
-# with open('./data/uber_2021.pdf','r') as txt:
-#      context = txt.read()
-
 loader = PyPDFLoader('./data/dados.pdf')
-pages = loader.load_and_split()
 
-vector_store = Milvus.from_documents(documents=pages,embedding=embeddings,connection_args={"uri":"./milvus_articles.db",},drop_old=True)
+# Configurando o TextSplitter
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=300)
+
+# Carregando e dividindo o PDF usando o TextSplitter
+pages = loader.load_and_split(text_splitter=text_splitter)
+
+vector_store = Milvus.from_documents(documents=pages, embedding=embeddings, connection_args={"uri":"./milvus_articles.db",}, drop_old=True)
 
 llm = Ollama(
     model='llama3',
     temperature=0
 )
 
-prompt = PromptTemplate(
-    input_variables=["context","question"],
-    template = """
-    # INSTRUÇÃO
-    Você é um atendente e deve esclarecer as dúvidas enviadas pelo usuário.
-    Se a pergunta não estiver relacionada a Amamentação materna ou não estiver nos dados do contexto, responda amigavelmente que só pode ajudar com dúvidas nesse contexto.
+def getBetterPromptByDistance(distance):
+    
+    prompt = ""
 
-    # CONTEXTO PARA RESPOSTAS
-    {context}
+    if 0.9 <= distance <= 1.05:
 
-    # PERGUNTA
-    Pergunta: {question}
-    """
-)
+        prompt = PromptTemplate(
+            input_variables=["context","question"],
+            template = """
+            # INSTRUÇÃO
+            Você é um atendente e deve esclarecer as dúvidas enviadas pelo usuário.
+            Junto com sua resposta deixe claro que ela não é uma resposta que pode conter inconsistências
 
-retriever = vector_store.as_retriever()
+            # CONTEXTO PARA RESPOSTAS
+            {context}
 
-rag_chain = (
-    {"context": retriever, "question": RunnablePassthrough()} 
-    | prompt
-    | llm
-    | StrOutputParser()
+            # PERGUNTA
+            Pergunta: {question}
+            """
+        )
+    
+    else:
 
-)
+        prompt = PromptTemplate(
+            input_variables=["context","question"],
+            template = """
+            # INSTRUÇÃO
+            Você é um atendente e deve esclarecer as dúvidas enviadas pelo usuário.
+
+            # CONTEXTO PARA RESPOSTAS
+            {context}
+
+            # PERGUNTA
+            Pergunta: {question}
+            """
+        )
+
+    return prompt
+
+#retriever = vector_store.as_retriever()
+
+def calculation_distance(pergunta):
+    results = vector_store.similarity_search_with_score(pergunta, k=1)
+    distance = 0
+    for content in results:
+        distance = content[-1]
+    return distance
+
+def get_context(pergunta):
+    # Usa o similarity_search para recuperar o contexto mais relevante
+    results = vector_store.similarity_search(pergunta, k=1)
+    # Retorna o conteúdo do primeiro resultado
+    return results[0].page_content if results else ""
 
 st.title("Chat Amamentação")
 
@@ -66,30 +98,41 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-
-if pergunta:= st.chat_input("Faça uma pergunta sobre amamentação para o assistente"):
+if pergunta := st.chat_input("Faça uma pergunta sobre amamentação para o assistente"):
     with st.chat_message("user"):
         st.markdown(pergunta)
-        st.session_state.messages.append({"role":"user","content":pergunta})
+        st.session_state.messages.append({"role": "user", "content": pergunta})
 
-    resposta = rag_chain.invoke(pergunta)
+    distance = calculation_distance(pergunta)
 
-    with st.chat_message("assistant"):
-        st.markdown(resposta)
-        #print(resposta)
-        st.session_state.messages.append({"role":"assistant","content":str(resposta)})
+    if distance > 1.05:
+        resposta = "Não tenho conhecimento para responder a essa pergunta"
+
+        with st.chat_message("assistant"):
+
+            st.markdown(resposta)
+            st.session_state.messages.append({"role": "assistant", "content": str(resposta)})
     
+    else:
 
+        # Recupera o contexto mais relevante
+        context = get_context(pergunta)
 
+        prompt = getBetterPromptByDistance(distance)
 
-    
-    
+        print(distance)
 
+        rag_chain = (
+            {"context": RunnablePassthrough(), "question": RunnablePassthrough(), "distance": RunnablePassthrough()} 
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
 
-# while True:
+        # Passa 'context' e 'question' para o invoke
+        resposta = rag_chain.invoke({"context": context, "question": pergunta})
 
-#     pergunta = input("Faça sua pergunta ao assistente: ")
-#     resumo = rag_chain.invoke(pergunta)
-#     print(f"Resposta: {resumo}")
-#     print("------------------------------ \n\n")
+        with st.chat_message("assistant"):
 
+            st.markdown(resposta)
+            st.session_state.messages.append({"role": "assistant", "content": str(resposta)})
