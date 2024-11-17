cf0b88a743905f37ba5f1e99ed1d1f661be92307
Paulo Meneghini <paulo.meneghini@outlook.com>
2024-11-16 21:05:29 -0300

    separating milvus from chat and making it read more than one file to the database

--- chat.py
+++ chat.py
@@ -8,27 +8,10 @@ from langchain_community.llms import Ollama
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-import streamlit as st
 
-#pip install langchain-huggingface
-from langchain_huggingface import HuggingFaceEmbeddings
+from milvus import initialize_milvus
 
-embeddings = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
-from langchain_community.document_loaders import PyPDFLoader
-
-#%pip install -qU  langchain_milvus
-from langchain_milvus import Milvus
-
-loader = PyPDFLoader('./data/dados.pdf')
-
-# Configurando o TextSplitter
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=300)
-
-# Carregando e dividindo o PDF usando o TextSplitter
-pages = loader.load_and_split(text_splitter=text_splitter)
-
-vector_store = Milvus.from_documents(documents=pages, embedding=embeddings, connection_args={"uri":"./milvus_articles.db",}, drop_old=True)
+vector_store = initialize_milvus()
 
 llm = Ollama(
     model='llama3',
@@ -40,15 +23,6 @@ def get_contexts(pergunta):
 
     # Criar um novo array com os page_content e score menor 1
     relevantDocuments = [(document.page_content, score) for document, score in results if score < 1]
-
-    # Exibir o novo array
-    # if len(relevantDocuments) == 0:
-    #     print("relevantDocuments está vazio")
-    # else:
-    #     for i, (page_content, score) in enumerate(relevantDocuments, start=1):
-    #         print(f"Item {i}:")
-    #         print(f"  Page Content: {page_content}")
-    #         print(f"  Score: {score}")
 
     print(f"Tamanho relevantDocuments {len(relevantDocuments)}")
 
@@ -87,7 +61,6 @@ def process_question(question):
             | StrOutputParser()
         )
 
-        #Passa 'context' e 'question' para o invoke
         resposta = rag_chain.invoke({"context": contexts, "question": question})
 
         if len(contexts) < 4:
